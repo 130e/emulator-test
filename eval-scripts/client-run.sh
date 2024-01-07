5f770905ac42dfe6ee52f598c0eb9bdfddd12fde
130e <fernival328@gmail.com>
2024-01-07 00:47:29 -0800

    add retest in script

--- eval-scripts/client-run.sh
+++ eval-scripts/client-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 iperf3=iperf3 # at least 3.9
+algo="westwood"
 
 downtown=({1..6})
 parkinglot=(7)
@@ -11,21 +12,29 @@ seaport=(12 13)
 labels=('A' 'B' 'C' 'D' 'E' )
 locations=(downtown parkinglot sportpark airport seaport)
 
-for loc_idx in "${!locations[@]}"; do
+for loc_idx in "${!locations[@]}"
+do
     echo "location ${labels[$loc_idx]}"
     declare -n traces=${locations[$loc_idx]}
-    for id in "${traces[@]}"; do
-        fname="./replay/cl-$id-westwood.log"
-        echo "prepare to test $fname"
-        read ans
-
-        if [ -z ${ans} ]; then
-            echo "skipped"
-        elif [ $ans = "y" ]; then
-            $iperf3 -s -i 0.1 -J --logfile $fname -1
-            echo "Waiting for key press to clear log and restart..."
-        else
-            echo "unkonwn input. Skipped."
-        fi
+    for id in "${traces[@]}"
+    do
+        outputfname="./replay/cl-$id-$algo.log"
+        newtest=false
+        while true
+        do
+            read -p "[client] Ready to test $outputfname; test? y/N" ans
+            if ! [ -z "${ans}" ] && [ "${ans}" = "y" ]
+            then
+                $iperf3 -s -i 0.1 -J --logfile $outputfname -1
+                newtest=true
+            else
+                echo "Skipped"
+                break
+            fi
+            #if [ newtest ]
+            #then
+                #mv "./sv-test-c.log ./replay/sv-$fid-$algo.log"
+            #fi
+        done
     done
 done
--- eval-scripts/server-run.sh
+++ eval-scripts/server-run.sh
@@ -13,23 +13,30 @@ seaport=(12 13)
 labels=('A' 'B' 'C' 'D' 'E' )
 locations=(downtown parkinglot sportpark airport seaport)
 
-for loc_idx in "${!locations[@]}"; do
+for loc_idx in "${!locations[@]}"
+do
     echo "location ${labels[$loc_idx]}"
     declare -n traces=${locations[$loc_idx]}
-    for id in "${traces[@]}"; do
+    for id in "${traces[@]}"
+    do
         inputfname="./input/trace-$id-c.csv"
         outputfname="./replay/sv-$id-$algo.log"
-        echo "prepare to test $inputfname and generate $outputfname"
-        read ans
-
-        if [ -z ${ans} ]; then
-            echo "skipped"
-        elif [ $ans = "y" ]; then
-            $exe $inputfname
-            echo "Waiting for key press to clear log and restart..."
-            mv "./replay/sv-test-c.log ./replay/sv-$fid-$algo.log"
-        else
-            echo "unkonwn input. Skipped."
-        fi
+        newtest=false
+        while true
+        do
+            read -p "[server] Ready to test $inputfname and generate $outputfname; test? y/N" ans
+            if ! [ -z "${ans}" ] && [ "${ans}" = "y" ]
+            then
+                $exe $inputfname
+                newtest=true
+            else
+                echo "Skipped"
+                break
+            fi
+            if [ newtest ]
+            then
+                mv "./sv-test-c.log ./replay/sv-$fid-$algo.log"
+            fi
+        done
     done
 done
